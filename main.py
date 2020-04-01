fe23ed8567e7b9bb9bfd805679dd319859af3033
tm3k <60632198+tm3k@users.noreply.github.com>
2020-04-01 15:37:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ import oandapyV20.endpoints.accounts as accounts
 import oandapyV20.endpoints.instruments as instruments
 import tweepy as tweepy
 import eur_usd
+import usd_cad
 
 
-# Prints eurusd trade dataframe
+## Prints trade dataframe(s) for user to interpret
 eur_usd.trade_eurusd()
+usd_cad.trade_usdcad()
